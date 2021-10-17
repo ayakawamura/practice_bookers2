3cd67c4ec0b262048b3a1490a2d45f7528dd60b3
ayakawamura <ayakawamura.b@gmail.com>
2021-10-17 08:15:47 +0000

    ヘッダーフッターヘッダー作成

---
+++ app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ApplicationRecord
+  
+  belongs_to :user
+  
+  validates :title ,presence: true
+  validates :body ,presence: true ,length:{maximum: 200}
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  get 'sub/top'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "homes#top"
+  get "about" => "homes#about" 
+  
+  resources :users ,only:[:index,:show,:edit,:update]
+  resources :books
 end
